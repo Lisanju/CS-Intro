808bf33a067e61f983f544a1ba900f3ee49e8bb3
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-11-06 14:17:54 -0300

    Create list01.py

---
+++ exercises/list01.py
@@ -0,0 +1,36 @@
+#Ex 1#
+
+intn = int(input("Digite um número inteiro: "))
+print(intn)
+
+#Ex 2#
+
+rn = float(input("Digite um número real: "))
+print(rn)
+
+#Ex 3#
+
+int1 = int(input("Digite um número inteiro: "))
+int2 = int(input("Digite outro número inteiro: "))
+int3 = int(input("Digite mais um número inteiro: "))
+print(int1+int2+int3)
+
+#Ex 4#
+
+rn = float(input("Digite um número real: "))
+print(rn**2)
+
+#Ex 5#
+
+celsius_temp = input("Digite uma temperatura em graus Celsius: ")
+fahrenheit_temp = (celsius_temp*9)/5 + 32
+print("A temperatura em graus Fahrenheit é: ", fahrenheit_temp)
+
+#Ex 6#
+
+fahrenheit_temp = input("Digite uma temperatura em graus Fahrenheit: ")
+celsius_temp = (fahrenheit_temp*5)/9 - 32
+print("A temperatura em graus Celsius é: ", celsius_temp)
+
+#Ex 7#
+
