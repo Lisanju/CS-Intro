fd67170217496056b02243e2073441086c1aec34
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-11-06 14:36:49 -0300

    Update list01.py

--- exercises/list01.py
+++ exercises/list01.py
@@ -22,15 +22,80 @@ print(rn**2)
 
 #Ex 5#
 
-celsius_temp = input("Digite uma temperatura em graus Celsius: ")
-fahrenheit_temp = (celsius_temp*9)/5 + 32
-print("A temperatura em graus Fahrenheit é: ", fahrenheit_temp)
+rn = float(input("Digite um número real: "))
+print(rn/5)
 
 #Ex 6#
 
-fahrenheit_temp = input("Digite uma temperatura em graus Fahrenheit: ")
+celsius_temp = float(input("Digite uma temperatura em graus Celsius: "))
+fahrenheit_temp = (celsius_temp*9)/5 + 32
+print("A temperatura em graus Fahrenheit é: ", fahrenheit_temp)
+
+#Ex 7#
+
+fahrenheit_temp = float(input("Digite uma temperatura em graus Fahrenheit: "))
 celsius_temp = (fahrenheit_temp*5)/9 - 32
 print("A temperatura em graus Celsius é: ", celsius_temp)
 
-#Ex 7#
+#Ex 8#
+
+kelvin_temp = float(input("Digite uma temperatura em graus Kelvin: "))
+celsius_temp = kelvin_temp - 273.15
+print("A temperatura em graus Celsius é: ", celsius_temp)
+
+#Ex 9#
+
+celsius_temp = float(input("Digite uma temperatura em graus Celsius: "))
+kelvin_temp = celsius_temp + 273.15
+print("A temperatura em graus Kelvin é: ", kelvin_temp)
+
+#Ex 10#
+
+kmh_v = float(input("Digite uma velocidade em km/h: "))
+ms_v = kmh_v/3.6
+print("A velocidade em m/s é: ", ms_v)
+
+#Ex 11#
+
+ms_v = float(input("Digite uma velocidade em m/s: "))
+kmh_v = ms_v*3.6
+print("A velocidade em km/h é: ", kmh_v)
+
+#Ex 12#
+
+m_d = float(input("Digite uma distância em milhas: "))
+km_d = m_d*1.61
+print("A distância em km é: ", km_d)
+
+#Ex 13#
+
+km_d = float(input("Digite uma distância em km: "))
+m_d = km_d/1.61
+print(m_d)
+
+#Ex 14#
+
+g_ang = float(input("Digite um ângulo em graus: "))
+rad_ang = (g_ang*3.14)/180
+print("O ângulo em radianos é: ", rad_ang)
+
+#Ex 15#
+
+rad_ang = float(input("Digite um ângulo em radianos: "))
+g_ang = (rad_ang*180)/3.14
+print("O ângulo em graus é: ", g_ang)
+
+#Ex 16#
+
+p_comp = float(input("Digite um comprimento em polegadas: "))
+cm_comp = p_comp*2.54
+print("O comprimento em centímetros é: ", cm_comp)
+
+#Ex 17#
+
+cm_comp = float(input("Digite um comprimento em centímetros: "))
+p_comp = cm_comp/2.54
+print("O comprimento em polegadas é: ", p_comp)
+
+#Ex 18#
 
