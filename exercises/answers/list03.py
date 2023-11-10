864e3419b80597b1e74edb123087349fff36d148
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-11-10 10:37:49 -0300

    Create list03.py

---
+++ exercises/answers/list03.py
@@ -0,0 +1,47 @@
+#Ex 1#
+
+c = 1
+while(c<=5):
+    mult = 3*c
+    c = c+1
+    print(mult)
+
+print("Fim do programa")
+
+#Ex 2#
+
+c = 1
+while(c<=100):
+    print(c)
+    c = c +1
+
+print("Fim do programa while")
+
+n = 100
+c = 1
+for c in range(1,n+1):
+    print(c)
+
+print("Fim do programa for")
+
+#Ex 3#
+
+c = 10
+while(c>=0):
+    print(c)
+    c = c-1
+
+print("FIM!")
+
+#Ex 4#
+
+inicial = 0
+final = 100000
+contagem = 1000
+for n in range(inicial,final+1000,contagem):
+    print(n)
+
+print("Fim do programa")
+
+#Ex 5#
+
