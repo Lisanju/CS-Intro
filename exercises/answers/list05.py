06814632fbb1649d384fd998b805880058eedb50
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-08 17:36:21 -0300

    Update list05.py

--- exercises/answers/list05.py
+++ exercises/answers/list05.py
@@ -1,1 +1,26 @@
-# Ex 1 #
+#Ex 1#
+
+str = input("Insira um texto: ")
+print(str)
+
+#Ex 2#
+
+print(len(str))
+
+#Ex 3#
+
+nome = input("Insira um nome: ")
+print(nome[:4])
+
+#Ex 4#
+
+nome = input("Insira um nome: ")
+
+if (nome[0] == "a" or nome[0] == "A"):
+  print(nome)
+
+else:
+  print("O nome não começa com 'a'")
+
+#Ex 5#
+
