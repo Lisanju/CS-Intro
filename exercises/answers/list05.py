0eb3a8f25bb65b986082bab62c9c279d1d69789c
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-09 13:32:07 -0300

    Update list05.py

--- exercises/answers/list05.py
+++ exercises/answers/list05.py
@@ -24,3 +24,38 @@ else:
 
 #Ex 5#
 
+str = input("Insira uma palavra: ")
+str_nova = ""
+
+for indice in range(len(str)):
+    conv = ord(str[indice])
+    conv = conv + 1
+    str_nova = str_nova + chr(conv)
+    
+print("Resultado: ", str_nova)
+
+#Ex 6#
+
+str1 = input("Insira uma palavra: ")
+str2 = input("Insira outra palavra: ")
+
+if (str1 < str2):
+    print("A palavra ", str1, " vem antes de ", str2, " na ordem alfabética")
+    
+else:
+    print("A palavra ", str2, " vem antes de ", str1, " na ordem alfabética")
+
+#Ex 7#
+
+str_mai = input("Digite uma palavra em maiúsculo: ")
+str_min = ""
+
+for indice in range(len(str_mai)):
+    conv = ord(str_mai[indice])
+    conv = conv + 32
+    str_min = str_min + chr(conv)
+    
+print(str_min)
+
+#Ex 8#
+
