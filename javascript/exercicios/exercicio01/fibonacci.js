534d5d4ff42017b33f260e0119da0b3568725736
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-13 10:56:55 -0300

    Update fibonacci.js

--- javascript/exercicios/exercicio01/fibonacci.js
+++ javascript/exercicios/exercicio01/fibonacci.js
@@ -1,1 +1,23 @@
+var n = prompt("Insira um valor N: ");
+var num1 = 0;
+var num2 = 1;
 
+if(n == 1){
+  console.log(0);
+}
+else if(n == 2){
+  console.log(0);
+  console.log(1);
+}
+else{
+  console.log(0);
+  console.log(1);
+  contador = 3;
+  while(contador <= n){
+    fN = num1 + num2
+    num1 = num2;
+    num2 = fN;
+    contador++;
+    console.log(fN);
+  }
+}
