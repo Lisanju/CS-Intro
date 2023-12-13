0b0fb04522182311793cc00608b9d958a758aad5
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-13 10:09:13 -0300

    Update conversao.js

--- javascript/exercicios/exercicio01/conversao.js
+++ javascript/exercicios/exercicio01/conversao.js
@@ -1,1 +1,8 @@
+<script type="text/javascript">
+  var fahConvCel = c * 9/5 + 32;
 
+  for(c = -30; c <= 150; 10++){
+    console.log(c + " graus Celsius");
+    console.log(fahConvCel + " graus Fahrenheit");
+  }
+</script>
