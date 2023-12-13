7327688f43e108ef00fa539f74a13edbab312f25
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-13 12:41:59 -0300

    Update modf.js

--- javascript/exercicios/exercicio02/modf.js
+++ javascript/exercicios/exercicio02/modf.js
@@ -1,1 +1,18 @@
-
+<DOCTYPE! html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <style type="text/javascript">
+        window.onload = function(){
+          id.mudar.style.fontSize = "10px";
+      </style>
+    </head>
+    <body>
+      <ul>
+        <li id="mudar">Mudar fonte para 30px</li>
+        <li id="cor">Mudar a cor</li>
+        <li id="fundo">Fundo preto, texto branco</li>
+        <li id="aumentar">Aumentar fonte em 5px</li>
+      </ul>
+    </body>
+  </html>
