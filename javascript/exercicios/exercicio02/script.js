928603444a3cbcfd9a448adaa453ae898c917325
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-13 13:08:54 -0300

    Create script.js

---
+++ javascript/exercicios/exercicio02/script.js
@@ -0,0 +1,6 @@
+var texto = document.getelementsByTagName("p")[0];
+
+texto.style.fontSize = "30px";
+texto.style.color = "white";
+texto.style.backgroundColor = "black";
+texto.style.fontSize = "35px";
