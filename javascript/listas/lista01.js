09494e6a53ec2e6f3cc8a8cd92f48054550031b5
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-17 19:59:57 -0300

    Update lista01.js

--- javascript/listas/lista01.js
+++ javascript/listas/lista01.js
@@ -26,3 +26,43 @@ medirTamanho("nana");
 
 /*Ex 4*/
 
+function gritarNome(name){
+    console.log(name.toUpperCase());
+    return name.toUpperCase();
+}
+
+gritarNome("nana");
+
+/*Ex 5*/
+
+function falaBaixo(name){
+    console.log(name.toLowerCase());
+    return name.toLowerCase();
+}
+
+falaBaixo("NANA");
+
+/*Ex 6*/
+
+function primeiroChar(str){
+    var primeiro = str[0];
+    console.log(primeiro);
+    return primeiro;
+}
+
+var gato = "nana";
+primeiroChar(gato);
+
+/*Ex 7*/
+
+function ultimoChar(str){
+    var ultimo = str.at(-1);
+    console.log(ultimo);
+    return ultimo;
+}
+
+var gato = "nana";
+ultimoChar(gato);
+
+/*Ex 8*/
+
