7d0c370c0047d35bd7ab0083200aa23fe58b7b0e
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-17 19:48:23 -0300

    Update lista01.js

--- javascript/listas/lista01.js
+++ javascript/listas/lista01.js
@@ -7,3 +7,22 @@ function soma(a,b){
 soma(1,3);
 
 /*Ex 2*/
+
+function mult(a,b){
+  console.log(a*b);
+  return a*b;
+};
+
+mult(3,2);
+
+/*Ex 3*/
+
+function medirTamanho(str){
+    console.log(str.length);
+    return str.length;
+}
+
+medirTamanho("nana");
+
+/*Ex 4*/
+
