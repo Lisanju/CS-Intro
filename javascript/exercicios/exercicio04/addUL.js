6e80dea3d07247ed59e916fa93b6fb2e6561c386
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-19 08:05:46 -0300

    Create addUL.js

---
+++ javascript/exercicios/exercicio04/addUL.js
@@ -0,0 +1,8 @@
+let novaUl = document.createElement("ul");
+
+novaUl.innerHTML = `<li>Pêssego</li>
+                    <li>Uva</li>
+                    <li>Maçã</li>
+                    <li>Limão</li>`;
+
+document.body.appendChild(novaUl);
