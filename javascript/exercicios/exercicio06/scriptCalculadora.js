3b6999e280c96ab80b8e8f3316d24c95c597a5dc
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-19 10:08:29 -0300

    Update scriptCalculadora.js

--- javascript/exercicios/exercicio06/scriptCalculadora.js
+++ javascript/exercicios/exercicio06/scriptCalculadora.js
@@ -1,29 +1,28 @@
-$(document).ready(function(){
-  const main = document.getElementsByTagName("main");
-  let armazenador = "";
-  let resultado = "";
+$(document).ready(function() {
+    const main = document.getElementsByTagName("main");
+    let armazenador = "";
+    let resultado = "";
 
-  let novaCelulaLogo = $("<div>").text("Resultado:");
-  $("main").append(novaCelulaLogo);
+    let novaCelulaLogo = $("<div>").text("Resultado:");
+    $("main").append(novaCelulaLogo);
 
-  let novaCelulaResultado = $("<div>").text("");
-  $("main").append(novaCelulaResultado);
-  
-  $("div").click(function(e){
-    if ($(this).text() !== "=" && $(this).text() !== "AC"){
-      armazenador += "" + $(this).text();
-      novaCelulaResultado.innerText += armazenador;
-    };
+    let novaCelulaResultado = $("<div>").text("");
+    $("main").append(novaCelulaResultado);
 
-    else if ($(this).text() === "AC"){
-      armazenador = "";
-      novaCelulaResultado.innerText = "";
-    };
+    $("div").click(function(e) {
+        if ($(this).text() !== "=" && $(this).text() !== "AC") {
+            armazenador += "" + $(this).text();
+            novaCelulaResultado.innerText += armazenador;
 
-    else{
-      resultado = eval(armaEzenador);
-      armazenador = "";
-      novaCelulaResultado.innerText = resultado;
-    };
-  });
+        } else {
+            if ($(this).text() === "AC") {
+                armazenador = "";
+                novaCelulaResultado.text() = "";
+            } else {
+                resultado = eval(armazenador);
+                armazenador = "";
+                novaCelulaResultado.text() = resultado;
+            }
+        }
+    });
 });
