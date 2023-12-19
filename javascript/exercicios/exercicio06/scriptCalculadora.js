22cff9e49c87f283c14088901e2ebb5bb7dfd11a
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-19 09:37:53 -0300

    Update scriptCalculadora.js

--- javascript/exercicios/exercicio06/scriptCalculadora.js
+++ javascript/exercicios/exercicio06/scriptCalculadora.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
   let armazenador = "";
   let resultado = "";
+
+  let novaCelulaLogo = document.createElement("div");
+  novaCelulaLogo.innerText = "Resultado:"
+  document.main.appendChild(novaCelulaLogo);
+
+  let novaCelulaResultado = document.createElement("div");
+  novaCelulaResultado.innerText = "";
+  novaCelulaResultado.main.appendChild(novaCelulaResultado);
   
   $("div").click(function(e){
     if (e.div.innerText !== "=" && e.div.innerText !== "AC"){
-      armazenador += e.div.innerText;
+      armazenador += "" + e.div.innerText;
+      novaCelulaResultado.innerText += armazenador;
     };
 
     else if (e.div.innerText === "AC"){
       armazenador = "";
+      novaCelulaResultado.innerText = "";
     };
 
     else{
       resultado = eval(armazenador);
       armazenador = "";
-      
-      return resultado;
+      novaCelulaResultado.innerText = eval(resultado);
     };
   });
 });
