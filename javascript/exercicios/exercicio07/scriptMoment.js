7b0cb28c7ce6bdf8ea828c8e48746078a86063a7
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-19 11:39:25 -0300

    Update scriptMoment.js

--- javascript/exercicios/exercicio07/scriptMoment.js
+++ javascript/exercicios/exercicio07/scriptMoment.js
@@ -1,1 +1,9 @@
+moment.locale("pt-br");
+const anoAtual = moment(YY);
+
+let inicio = moment("2023-01-01 00:00:00");
+inicio.year(anoAtual);
+
+let final = moment("2023-12-31 23:59:59");
+final.year(anoAtual);
 
