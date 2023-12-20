e80b99e1f9df221bae914c5fe3dee129b5c5e0a3
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-20 14:51:39 -0300

    Update script.js

--- javascript/exercicios/exercicio08/script.js
+++ javascript/exercicios/exercicio08/script.js
@@ -3,11 +3,26 @@ $(document).ready(function(){
     return parseFloat($("p").css("font-size"));
   };
   
-  $("#fMais").click(function(e){
-    $("p").attr("font-size", obterTamanho() + 1);
+  const aumentar = $("#fMais");
+  const diminuir = $("#fMenos");
+  const noturno = $("#mNoturno");
+  const diurno = $("#mDiurno");
+
+  aumentar.on("click", function(){
+    $("p").css("font-size", obterTamanho() + 1);
   });
 
-  $("#fMenos").click(function(e){
-    $("p").attr("font-size", obterTamanho() - 1);
+  diminuir.on("click", function(){
+    $("p").css("font-size", obterTamanho() - 1);
   });
+
+  noturno.on("click", function(){
+    $("body").css("background-color", "black");
+    $("p").css("color", "white");
+  })
+
+  diurno.on("click", function(){
+    $("body").css("background-color", "white");
+    $("p").css("color", "black");
+  })
 });
