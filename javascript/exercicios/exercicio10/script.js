f4dccad6fc2d1345fd309167f6c34ee363f3acae
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-20 19:41:20 -0300

    Update script.js

--- javascript/exercicios/exercicio10/script.js
+++ javascript/exercicios/exercicio10/script.js
@@ -6,14 +6,38 @@ $(document).ready(function(){
   var linhaPotencia = $("#potencia");
   var linhaPreco = $("#preco");
   var linhaAcessorios = $("#acessorios");
-
-  
   
   function tratamento(dados){
     $.each(dados, function(chave, valor){
-      
+      let td = $("<td>");
+      td.html(valor);
+      if (chave === "nome"){
+        linhaNome.append(td);
+      } else {
+        if (chave === "fabricante"){
+          linhaFabricante.append(td);
+        } else {
+          if (chave === "ano"){
+            linhaAno.append(td);
+          } else {
+            if (chave === "potencia"){
+              linhaPotencia.append(td);
+            } else {
+              if (chave === "preco"){
+                linhaPreco.append(td);
+              } else {
+                let ul = $("<ul>");
+                let li = $("<li>");
+                for (let i = 0; i < valor.length; i++){
+                li.html(valor[i]);
+                ul.append(li);
+                }
+              }
+            }
+          }
+        }
+      }
     });
   };
-  
   $.getJSON("carros.json", tratamento);
 });
