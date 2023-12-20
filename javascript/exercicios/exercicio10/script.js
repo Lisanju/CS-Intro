f2ab5113c1d12c2bbdf24855cc833326f08b4681
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-20 20:21:32 -0300

    Update script.js

--- javascript/exercicios/exercicio10/script.js
+++ javascript/exercicios/exercicio10/script.js
@@ -1,43 +1,30 @@
 $(document).ready(function(){
+  var carros = [
+    {
+      "nome": "Gol",
+      "fabricante": "Volkswagen",
+      "ano": 2009,
+      "potencia": 3000,
+      "preco": 150000,
+      "acessorios": ["banco", "teto", "boneco"]
+    },
+    {
+      "nome": "Mobi",
+      "fabricante": "Fiat",
+      "ano": 2014,
+      "potencia": 4500,
+      "preco": 35000,
+      "acessorios": ["janela", "pneu-extra", "macaco"]
+    },
+    {
+      "nome": "Onix",
+      "fabricante": "Chevrolet",
+      "ano": 2019,
+      "potencia": 5000,
+      "preco": 500000,
+      "acessorios": ["asa", "turbo", "espinho"]
+    }
+  ];
 
-  var linhaNome = $("#nome");
-  var linhaFabricante = $("#fabricante");
-  var linhaAno = $("#ano");
-  var linhaPotencia = $("#potencia");
-  var linhaPreco = $("#preco");
-  var linhaAcessorios = $("#acessorios");
-  
-  function tratamento(dados){
-    $.each(dados, function(chave, valor){
-      let td = $("<td>");
-      td.html(valor);
-      if (chave === "nome"){
-        linhaNome.append(td);
-      } else {
-        if (chave === "fabricante"){
-          linhaFabricante.append(td);
-        } else {
-          if (chave === "ano"){
-            linhaAno.append(td);
-          } else {
-            if (chave === "potencia"){
-              linhaPotencia.append(td);
-            } else {
-              if (chave === "preco"){
-                linhaPreco.append(td);
-              } else {
-                let ul = $("<ul>");
-                let li = $("<li>");
-                for (let i = 0; i < valor.length; i++){
-                li.html(valor[i]);
-                ul.append(li);
-                }
-              }
-            }
-          }
-        }
-      }
-    });
-  };
-  $.getJSON("carros.json", tratamento);
+
 });
