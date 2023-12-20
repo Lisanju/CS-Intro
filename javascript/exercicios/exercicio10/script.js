4bcb88ad163d7c9edef350b25242153dc335038a
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-20 20:37:25 -0300

    Update script.js

--- javascript/exercicios/exercicio10/script.js
+++ javascript/exercicios/exercicio10/script.js
@@ -1,36 +1,48 @@
 $(document).ready(function(){
   var carros = [
     {
-      "nome": "Gol",
-      "fabricante": "Volkswagen",
-      "ano": 2009,
-      "potencia": 3000,
-      "preco": 150000,
-      "acessorios": ["banco", "teto", "boneco"]
-    },
-    {
-      "nome": "Mobi",
-      "fabricante": "Fiat",
-      "ano": 2014,
-      "potencia": 4500,
-      "preco": 35000,
-      "acessorios": ["janela", "pneu-extra", "macaco"]
-    },
-    {
-      "nome": "Onix",
-      "fabricante": "Chevrolet",
-      "ano": 2019,
-      "potencia": 5000,
-      "preco": 500000,
-      "acessorios": ["asa", "turbo", "espinho"]
-    }
-  ];
+    "nome": "Gol",
+    "fabricante": "Volkswagen",
+    "ano": 2009,
+    "potencia": 3000,
+    "preco": 150000,
+    "acessorios": [
+      "banco",
+      "teto",
+      "boneco"
+    ]
+  },
+  {
+    "nome": "Mobi",
+    "fabricante": "Fiat",
+    "ano": 2014,
+    "potencia": 4500,
+    "preco": 35000,
+    "acessorios": [
+      "janela",
+      "pneu-extra",
+      "macaco"
+    ]
+  },
+  {
+    "nome": "Onix",
+    "fabricante": "Chevrolet",
+    "ano": 2019,
+    "potencia": 5000,
+    "preco": 500000,
+    "acessorios": [
+      "asa",
+      "turbo",
+      "espinho"
+    ]
+  }
+]
 
   function adicionarLinha(carro){
-    var acessorios = "<ul>;
+    var acessorios = "<ul>";
 
     $.each(carro.acessorios, function(chave, valor){
-      acessorios += "<li>" + acessorio + "</li>";
+      acessorios += "<li>" + valor + "</li>";
     });
 
     acessorios += "</ul>";
