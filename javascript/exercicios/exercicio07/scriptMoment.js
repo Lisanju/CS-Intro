cb873c9a2d3de4795129fa02c01a03b84d1b39f9
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-20 10:58:29 -0300

    Update scriptMoment.js

--- javascript/exercicios/exercicio07/scriptMoment.js
+++ javascript/exercicios/exercicio07/scriptMoment.js
@@ -1,13 +1,20 @@
 window.onload = function(){
   moment.locale("pt-br");
   
+  const agora = moment(); // momento atual
+  
   const começo = moment({M:1, d:1, h:00, m:00, s:00});
   const final = moment({M:12, d:31, h:23, m:59, s:59});
-  let começoP = começo.fromNow(true);
-  let finalF = final.toNow(true);
   
-  let passou = document.getElementsById("passou");
-  let falta = document.getElementsById("falta");
+  let começoP = agora.diff(começo, 'seconds');
+  let finalF = final.diff(agora, 'seconds');
+  
+  começoP = moment.duration(começoP);
+  finalF = moment.duration(finalF);
+  
+  let passou = document.getElementById("passou");
+  let falta = document.getElementById("falta");
+  
   passou.innerHTML = começoP.format("MMMM [meses, ] DD [dias, ] hh [horas e ] mm [minutos]");
   falta.innerHTML = finalF.format("MMMM [meses, ] DD [dias, ] hh [horas e ] mm [minutos]");
 };
