feff60390a16994807b894e1856758d417bed0ba
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2023-12-21 16:59:53 -0300

    Update script.js

--- javascript/exercicios/exercicio12/script.js
+++ javascript/exercicios/exercicio12/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-  
+    const bPadrao = $(".bPadrao");
+    const bDeletar = $(".bDeletar");
+    const bExibir = $(".bExibir");
+
+    bPadrao.on("mouseover", function(e){
+        $(this).css({"border":"4px","border-color":"skyblue","border-style":"solid"}); 
+    });
+
+    bPadrao.on("mouseout", function(e){
+        $(this).css({"border":"4px","border-color":"white","border-style":"solid"}); 
+    });
+
+    bDeletar.on("mouseover", function(e){
+        $(this).css({"border":"4px","border-color":"indianred","border-style":"solid"}); 
+    });
+
+    bDeletar.on("mouseout", function(e){
+        $(this).css({"border":"4px","border-color":"white","border-style":"solid"}); 
+    });
+
+    bExibir.on("mouseover", function(e){
+        $(this).css({"border":"4px","border-color":"lightgreen","border-style":"solid"}); 
+    });
+
+    bExibir.on("mouseout", function(e){
+        $(this).css({"border":"4px","border-color":"white","border-style":"solid"}); 
+    });
 });
