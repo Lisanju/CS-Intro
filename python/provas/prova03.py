b9a9aad6f791644d2d64fc246a60710bea4969a4
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-01-30 09:52:12 -0300

    Update prova03.py

--- python/provas/prova03.py
+++ python/provas/prova03.py
@@ -1,1 +1,29 @@
+'''
+Implemente o jogo da forca. Ao iniciar o jogo, o seguinte menu de opções deverá aparecer para o usuário:
 
+1 - Gerenciar arquivo de palavras
+2 - Jogar
+3 - Sair
+
+Ao selecionar a opção “Gerenciar arquivo de palavras”, um novo menu de opções é apresentado
+
+1 - Adicionar palavra: adiciona uma nova palavra digitada pelo usuário ao final do
+arquivo “palavras.txt”. O arquivo não deve conter palavras duplicadas. As
+palavras são armazenadas uma por linha.
+2 - Excluir palavra: exclui a palavra digitada pelo usuário do arquivo
+“palavras.txt”.
+3 - Voltar: volta ao menu anterior
+
+Ao selecionar a opção “Jogar”, o jogo tem início:
+
+1 - Uma palavra é sorteada aleatoriamente do arquivo.
+2 - O programa apresenta ao jogador quantas letras a palavra possui
+3 - O jogador escolhe uma letra que ele acha que a palavra possua.
+4 - O programa informa se a letra existe ou não. As seguintes informações são apresentadas: as letras já tentadas, a quantidade de erros cometidos e a letras
+já reveladas da palavra oculta.
+5 - O jogador é declarador vencedor se conseguir revelar a palavra com menos de 5 erros
+
+Ao selecionar a opção “Sair”, o jogo termina.
+'''
+
+test()
