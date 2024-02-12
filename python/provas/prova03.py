6203eacdca3226ce2931d855fd4ec5dfded6b5ec
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-12 15:53:23 -0300

    Update prova03.py

--- python/provas/prova03.py
+++ python/provas/prova03.py
@@ -1,1 +1,18 @@
+# 1 - Faça um programa no qual o usuário informa o nome do arquivo e uma palavra, e retorne o número de vezes que aquela palavra aparece no arquivo. #
+
+# 2 - Faça um programa que receba do usuário um arquivo texto. Crie outro arquivo texto contendo o texto do arquivo de entrada, mas com as vogais substituídas por "*". #
+
+# 3 - Escreva um programa que leia um número inteiro positivo n e em seguida imprina n linhas do chamado Triangulo de Floyd.
+# Para n = 6, temos:
+#    1
+#    2 3
+#    4 5 6
+#    7 8 9 10
+#    11 12 13 14 15     
+#    16 17 18 19 20 21
+
+# 4 - Faça uma função recursiva que calcule e retorne o N-ésimo termo da sequência Fibonacci. Alguns números desta sequência são: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89... #
+
+# 5 - Faça um programa que calcule e escreva o valor de S: 
+#      S = 1/1 + 3/2 + 5/3 + 7/4 ... 99/50
 
