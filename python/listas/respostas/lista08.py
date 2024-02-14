bf3fcb23a7b488cd57a594c1fdfa71b8f16031d8
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-14 20:18:09 -0300

    Update lista08.py

--- python/listas/respostas/lista08.py
+++ python/listas/respostas/lista08.py
@@ -1,1 +1,25 @@
+#Ex 1#
+
+nomeArquivo = str(input("Digite o nome do arquivo: ")) + ".txt"
+arquivo = open(nomeArquivo,"r")
+listaLinhas = arquivo.readlines()
+
+print(f"O arquivo possui {len(listaLinhas)} linhas")
+
+#Ex 2#
+
+nomeArquivo = str(input("Digite o nome do arquivo: ")) + ".txt"
+arquivo = open(nomeArquivo,"r")
+texto = arquivo.read()
+
+qtVogais = 0
+
+for char in texto:
+    if (char == "a" or char == "e" or char == "i" or char == "o" or char == "u"):
+        qtVogais = qtVogais + 1
+
+print(f"O arquivo possui {qtVogais} vogais")
+
+#Ex 3#
+
 
