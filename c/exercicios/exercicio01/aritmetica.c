c80e3ffb2ee0a8b59461e2f7759907eb42526190
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-15 22:48:43 -0300

    Create aritmetica.c

---
+++ c/exercicios/exercicio01/aritmetica.c
@@ -0,0 +1,16 @@
+#include "aritmetica.h"
+int soma(int a,int b){
+    return a + b;
+}
+
+int subtracao(int a,int b){
+    return a - b;
+}
+
+int produto(int a,int b){
+    return a * b;
+}
+
+int divisao(int a,int b){
+    return a / b;
+}
