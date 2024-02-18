f4f07aa67e49594e9ac46ef1849c0436cfe4baff
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-18 17:16:56 -0300

    Create lista02.c

---
+++ c/listas/respostas/lista02.c
@@ -0,0 +1,19 @@
+// Ex 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,z;
+    printf("Digite um numero inteiro: ");
+    scanf("%d",&x);
+    y = x + 1;
+    z = x - 1;
+    printf("Antecessor de %d = %d\nSucessor de %d = %d\n",x,z,x,y);
+    system("pause");
+    return 0;
+}
+
+// Ex 2
+
