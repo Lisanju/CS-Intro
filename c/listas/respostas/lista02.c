61c5410106f70e0dd5381f785b9dfd7e9775ee40
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-18 17:42:21 -0300

    Update lista02.c

--- c/listas/respostas/lista02.c
+++ c/listas/respostas/lista02.c
@@ -113,3 +113,52 @@ int main()
 
 // Ex 8
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c,f;
+    printf("Digite uma temperatura em graus Celsius: \n");
+    scanf("%d",&c);
+    f = c * (9/5) + 32;
+    printf("Em graus Fahrenheit = %d\n",f);
+    system("pause");
+    return 0;
+}
+
+// Ex 9
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const float pi = 3.141592;
+int main()
+{
+    float g,r;
+    printf("Digite um angulo em graus: \n");
+    scanf("%f",&g);
+    r = g * (pi/180);
+    printf("Em radianos = %f\n",r);
+    system("pause");
+    return 0;
+}
+
+// Ex 10
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const float imp = 780000.00;
+int main()
+{
+    float g1,g2,g3;
+    g1 = imp * (46.0/100.0);
+    g2 = imp * (32.0/100.0);
+    g3 = imp - (g1 + g2);
+    printf("Ganhador 1 = %f\nGanhador 2 = %f\nGanhador 3 = %f\n",g1,g2,g3);
+    system("pause");
+    return 0;
+}
+
+// Ex 11
